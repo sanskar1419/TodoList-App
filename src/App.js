eb8292749fcf4e692001fb36d16155fca59d5f08
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-04-20 11:25:25 +0530

    Add Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl">Hii this is a todo app</h1>
-    </div>
+    <>
+      <RouterProvider />
+    </>
   );
 }
 
