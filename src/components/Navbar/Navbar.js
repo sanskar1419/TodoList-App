90db5afbe7d247e52ad0c0c26e3fa6c40c22cc00
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-04-20 13:04:42 +0530

    Modifing Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import addImage from "../../images/add.png";
 
 function Navbar() {
   return (
@@ -31,13 +32,10 @@ function Navbar() {
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-primary rounded-box w-52"
             >
               <li>
-                <a>Homepage</a>
+                <Link>Homepage</Link>
               </li>
               <li>
-                <a>Portfolio</a>
-              </li>
-              <li>
-                <a>About</a>
+                <Link>Add Todo</Link>
               </li>
             </ul>
           </div>
@@ -64,21 +62,10 @@ function Navbar() {
           </button>
           <button className="btn btn-ghost btn-circle">
             <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-              <span className="badge badge-xs badge-primary indicator-item"></span>
+              <Link>
+                {" "}
+                <img src={addImage} alt="Add" width="40px" />
+              </Link>
             </div>
           </button>
         </div>
