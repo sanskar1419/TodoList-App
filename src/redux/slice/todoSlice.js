0b54277f410b429b3aeb0fbf1e055a83d3327530
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-04-20 17:53:25 +0530

    Update Todo

--- src/redux/slice/todoSlice.js
+++ src/redux/slice/todoSlice.js
@@ -24,6 +24,25 @@ export const addTodoAsync = createAsyncThunk("todo/add", async (payload) => {
   return await response.json();
 });
 
+export const updateTodoAsync = createAsyncThunk(
+  "todo/update",
+  async (payload) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${payload.id}`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          userId: payload.userId,
+          title: payload.title,
+          completed: payload.completed,
+        }),
+      }
+    );
+    return await response.json();
+  }
+);
+
 const todoSlice = createSlice({
   name: "todo",
   initialState,
@@ -42,8 +61,23 @@ const todoSlice = createSlice({
         state.loading = false;
       })
       .addCase(addTodoAsync.fulfilled, (state, action) => {
-        state.todos.push(action.payload);
+        state.todos.unshift(action.payload);
+        state.loading = false;
         state.message = "Todo Added Successfully";
+      })
+      .addCase(updateTodoAsync.fulfilled, (state, action) => {
+        state.todos.map((todo) => {
+          if (
+            todo.id === action.payload.id &&
+            todo.userId === action.payload.userId
+          ) {
+            return action.payload;
+          }
+
+          return todo;
+        });
+        state.loading = false;
+        state.message = "Todo Updated Successfully";
       });
   },
 });
